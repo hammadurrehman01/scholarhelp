39250f76f7e1502f3e493f4606a6868b3631fdbc
hammadurrehman01 <hammadurrehman1954@gmail.com>
2024-11-07 12:25:13 +0500

    fix: loader foreground

--- src/app/components/common/Loader.tsx
+++ src/app/components/common/Loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="h-[100vh] flex items-center justify-center">
     <span
-      className="loader w-12 h-12 rounded-full border-t-4 border-r-4 border-white border-r-transparent inline-block "
+      className="loader w-12 h-12 rounded-full border-t-4 border-r-4 border-foreground border-r-transparent inline-block "
     ></span>
     </div>
   );
